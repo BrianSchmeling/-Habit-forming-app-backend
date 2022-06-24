66ffd6dbbe917fc39e85f4060d2965046bed56e0
Brian Schmeling <brianschmeling2@gmail.com>
2022-06-24 14:07:54 -0400

    Fix seed.js file

--- db/seed.js
+++ db/seed.js
@@ -14,5 +14,9 @@ Habits.deleteMany({}).then(() => {
         "Swimming, in recreation and sports, the propulsion of the body through water by combined arm and leg motions and the natural flotation of the body.",
       difficultyLevel: 3,
     },
-  ]);
+  ]).then((habit) => {
+    console.log(habit);
+    res.json(habit);
+    process.exit();
+  });
 });
