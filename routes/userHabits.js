481b8cdb615a66ac8a3bb5f12f2d9fce3b4a88ac
Artan <artanpl03@gmail.com>
2022-06-29 14:01:59 -0400

    changes to controller

--- routes/userHabits.js
+++ routes/userHabits.js
@@ -6,7 +6,7 @@ const userController = require("../controllers/userHabits")
 router.get('/', userController.index)
 router.get('/:id', userController.show)
 router.post('/', userController.create)
-router.put('/:id', userController.edit)
+router.put('/:user', userController.edit)
 router.delete('/:id', userController.delete)
 
 module.exports = router
