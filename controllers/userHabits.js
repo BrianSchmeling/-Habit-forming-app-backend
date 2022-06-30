f735764ab6ef6378bb47a8664f255b4a1a6dca4d
Brian Schmeling <brianschmeling2@gmail.com>
2022-06-30 10:18:49 -0400

    Ceployment chenges

--- controllers/userHabits.js
+++ controllers/userHabits.js
@@ -1,34 +1,31 @@
-const UserHabits = require("../models/UserHabits")
+const UserHabits = require("../models/UserHabits");
 
 module.exports = {
-    index: (req, res) => {
-        UserHabits.find()
-        .then(userhabit => {
-            res.json(userhabit)
-        })
-    },
-    show: (req, res) => {
-        UserHabits.findById(req.params.id)
-        .then(userhabit => {
-            res.json(userhabit)
-        })
-    },
-    create: (req, res) => {
-        UserHabits.create(req.body)
-        .then(userhabit => {
-            res.json(userhabit)
-        })
-    },
-    edit: (req, res) => {
-        UserHabits.findOneAndUpdate({habitName: req.params.habitName}, req.body, {new: true})
-        .then(userhabit => {
-            res.json(userhabit)
-        })
-    },
-    delete: (req, res) => {
-        UserHabits.findByIdAndDelete(req.params.id)
-        .then(userhabit => {
-            res.json(userhabit)
-        })
-    }
-}
+  index: (req, res) => {
+    UserHabits.find().then((userhabit) => {
+      res.json(userhabit);
+    });
+  },
+  show: (req, res) => {
+    UserHabits.findById(req.params.id).then((userhabit) => {
+      res.json(userhabit);
+    });
+  },
+  create: (req, res) => {
+    UserHabits.create(req.body).then((userhabit) => {
+      res.json(userhabit);
+    });
+  },
+  edit: (req, res) => {
+    UserHabits.findOneAndUpdate({ habitName: req.params.id }, req.body, {
+      new: true,
+    }).then((userhabit) => {
+      res.json(userhabit);
+    });
+  },
+  delete: (req, res) => {
+    UserHabits.findByIdAndDelete(req.params.id).then((userhabit) => {
+      res.json(userhabit);
+    });
+  },
+};
